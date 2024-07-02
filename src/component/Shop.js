0b6b228886d73a2fbe8d6b321bfe04aa3448978c
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-07-02 15:11:36 +0530

    first commit

---
+++ src/component/Shop.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <>
+      <div className="container">
+        <h1> Sports Shoes</h1>
+        <button className="btn btn-primary mx-3">-</button>
+        Add to cart
+        <button className="btn btn-primary mx-3">+</button>
+      </div>
+    </>
+  );
+};
+
+export default Shop;
