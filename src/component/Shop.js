375a0c395ee1059b2952b8c201b75bf999eaa1d4
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-07-02 16:50:00 +0530

    Updating state from a different component in Redux

--- src/component/Shop.js
+++ src/component/Shop.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+// import { bindActionCreators } from "redux";
+import { actionCreators } from "../state/index";
+import { bindActionCreators } from "redux";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  // const action = bindActionCreators(actionCreators,dispatch)
+  const {withdwawMoney, depositeMoney} = bindActionCreators(actionCreators,dispatch)
   return (
     <>
       <div className="container">
         <h1> Sports Shoes</h1>
-        <button className="btn btn-primary mx-3">-</button>
+        {/* <button className="btn btn-primary mx-3" onClick={() => {dispatch(actionCreators.withdwawMoney(100))}}>-</button>
         Add to cart
-        <button className="btn btn-primary mx-3">+</button>
+        <button className="btn btn-primary mx-3" onClick={() => {dispatch(actionCreators.depositeMoney(100))}}>+</button> */}
+        {/* <button className="btn btn-primary mx-3" onClick={() => {action.withdwawMoney(100)}}>-</button>
+        Add to cart
+        <button className="btn btn-primary mx-3" onClick={() => {action.depositeMoney(100)}}>+</button> */}
+        <button className="btn btn-primary mx-3" onClick={() => {withdwawMoney(100)}}>-</button>
+        Add to cart
+        <button className="btn btn-primary mx-3" onClick={() => {depositeMoney(100)}}>+</button>
       </div>
     </>
   );
